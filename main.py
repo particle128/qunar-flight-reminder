b10054b022ff340fc7a63f62d13d370d76e88861
Shu Ma <mashu128a@126.com>
2015-01-28 09:18:54 +0800

    encapsulate the code into a class and clean it

--- main.py
+++ main.py
@@ -1,123 +1,100 @@
 #!/usr/bin/env python
 # coding=utf-8
 
-import selenium
-from selenium import webdriver 
-#from selenium.webdriver.common.proxy import *
 import time
 import datetime
 import re
-from urlparse import urlparse
-import sys
 
-from config_local import *
-#from config import *
+import selenium
+from selenium import webdriver 
+
 from reminder import *
+from config import *
+# config_local.py will override the previous config.py
+try: 
+    from config_local import *
+except:
+    pass
+
+class FlightChecker():
+    def __init__(self):
+        self.driver = webdriver.Chrome()
 
 
-def check(driver):
-	"read the configuration file (config.py) once a time"
-	infoText=''
-	pos_pattern=re.compile('left: -(\d){2}px')
-	for demand in DEMANDS:
-		# read date info
-		start=demand['date_start'].split('-')
-		end=demand['date_end'].split('-')
-		date_start=datetime.date(int(start[0]),int(start[1]),int(start[2]))
-		date_end=datetime.date(int(end[0]),int(end[1]),int(end[2]))
+    def parse_date(self, date_str):
+        date_elements = date_str.split('-')
+        year = int(date_elements[0])
+        month = int(date_elements[1])
+        day = int(date_elements[2])
+        return datetime.date(year, month, day)
 
-		one_day=datetime.timedelta(days=1)# 1 day
 
-		dd=date_start
-		while dd<=date_end:
-			params={'':demand['src'],
-					'':demand['dst'],
-					'searchDepartureTime':dd.isoformat()}
-			driver.get("%s?searchDepartureAirport=%s&searchArrivalAirport=%s&searchDepartureTime=%s"%(URL,demand['src'],demand['dst'],dd.isoformat()))
-			
-			# in case that the page's ajax has not finished 
-			no_result=False
-			need_validation=False
-			threshold=10
-			time.sleep(10)
-			while True:
-				try:
-					driver.find_element_by_class_name('prc').find_elements_by_tag_name('b')
-				except Exception as e:
-					if driver.page_source.find(u'该航线当前无可售航班')!=-1:
-						no_result=True
-						break
-					threshold-=1
-					if threshold==0:
-						need_validation=True
-						break
-					print 'wait 10s '
-					time.sleep(10)
-				else:
-					break
+    def get_position(self, number_block):
+        "relative position of the price number"
+        pos_pattern = re.compile('left: -(\d){2}px')
+        target = str(number_block.get_attribute('style'))
+        return int(pos_pattern.search(target).group(1))
 
-			# no result under that day
-			if no_result:
-				print 'no result under'+dd.isoformat()+":"+demand['src']+" "+demand['dst']
-				dd+=one_day
-				continue
-			# validation page appears because of too frequent requests
-			if need_validation:
-				print 'too frequent,need varification code'
-				return
 
-			first=True
-			prc=[]
-			len=0
-			# only check the first price since the price is in an ascending sort order
-			try:
-				for block in driver.find_element_by_class_name('prc').find_elements_by_tag_name('b'):
-					if first:
-						prc=list(block.text)
-						# whole length of the price
-						len=int(pos_pattern.search(str(block.get_attribute('style'))).group(1))
-						first=False
-					else:
-						# position of the number
-						pos=int(pos_pattern.search(str(block.get_attribute('style'))).group(1))
-						prc[len-pos]=block.text
-				price=int(''.join(prc))
-				if price<=demand['price']:
-					infoText+="%s: %s to %s 's price is %d\r\n" %(dd.isoformat(),demand['src'],demand['dst'],price)
-					# if important information arrives, send email immediately
-					sendEmail(infoText)
-					print infoText
-					infoText=""
-			# if some exception occurs,let it go
-			except Exception as e:
-				print e
-					
-			# the next day 
-			dd+=one_day
-			# wait 30s
-			time.sleep(30)
+    def check_demand_without_except(self, cur_date_str):
+        # skip '邻近推荐' and choose the first element (lowest cost)
+        price_block = self.driver.find_element_by_id('hdivResultPanel').find_element_by_class_name('prc')
 
-if __name__=='__main__':
+        number_blocks = price_block.find_elements_by_tag_name('b')
+        number_list = list(number_blocks[0].text)
+        len = self.get_position(number_blocks[0])
 
-# attempt to use the http proxy, but fails.
+        for number_block in number_blocks[1:]:
+            pos = self.get_position(number_block)
+            number_list[len-pos] = number_block.text
 
-#	myProxy = "217.169.209.2:6666"
-#	proxy = Proxy({
-#		'proxyType': ProxyType.MANUAL,
-#		'httpProxy': myProxy,
-#		'ftpProxy': myProxy,
-#		'sslProxy': myProxy,
-#		'noProxy': '' # set this value as desired
-#		})
-	#driver = webdriver.Firefox(proxy=proxy)
+        price = ''.join(number_list)
+        print u'￥' + price
+        if int(price) <= self.price:
+            info = "%s: %s to %s 's price is %s\r\n" % (cur_date_str, self.src, self.dst, price)
+            send_email(info)
 
-	driver=webdriver.Chrome()
-	#sys.stdout=open('output','w')
-	print 'here we go'
-	while True:
-		# every 0.5h
-		print 'begin checking...'
-		check(driver)
-		print 'end checking...'
-		sys.stdout.flush() # flush the output
-		time.sleep(INTERVAL)
+
+    def check_demand(self, cur_date_str):
+        current_url = "%s?searchDepartureAirport=%s&searchArrivalAirport=%s&searchDepartureTime=%s" % (QUNAR_URL, self.src, self.dst, cur_date_str)
+        self.driver.get(current_url)
+        time.sleep(TIME_FOR_PAGE_LOAD_IN_SEC)
+        if self.driver.page_source.find(u'该航线当前无可售航班') != -1:
+            print 'no flight under ' + cur_date_str + ":" + self.src + " to " + self.dst
+            return
+        try:
+            self.check_demand_without_except(cur_date_str)
+        except Exception as e:
+            print e
+
+
+    def load_current_option(self, demand):
+        self.start_date = self.parse_date(demand['start_date'])
+        self.end_date = self.parse_date(demand['end_date'])
+        self.src = demand['src']
+        self.dst = demand['dst']
+        self.price = demand['price']
+        print self.src + ' to ' + self.dst
+
+
+    def check(self):
+        one_day = datetime.timedelta(days=1) # 1 day
+        for demand in DEMANDS:
+            self.load_current_option(demand)
+
+            cur_date = self.start_date
+            while cur_date <= self.end_date:
+                self.check_demand(cur_date.isoformat())
+                cur_date += one_day
+                time.sleep(INTERVAL_BETWEEN_TWO_DAYS_IN_SEC)
+ 
+                                    
+
+if __name__ == '__main__':
+    flightChecker = FlightChecker()
+    print 'here we go'
+    while True:
+        print 'begin checking...'
+        flightChecker.check()
+        print 'end checking...'
+        time.sleep(INTERVAL_BETWEEN_TWO_CHECKS_IN_SEC)
