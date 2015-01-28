6675cf680da4ad51bacbb24117d17894e10241e2
Shu Ma <mashu128a@126.com>
2015-01-28 09:20:22 +0800

    clean it

--- reminder.py
+++ reminder.py
@@ -4,21 +4,32 @@
 import smtplib
 from email.mime.text import MIMEText
 from email.charset import Charset
-from config_local import *
-#from config import *
 
-def sendEmail(text):
-	msg=MIMEText(text,_charset='utf-8') 
-	msg['From']=FROM_ADDR
-	msg['To']=TO_ADDR
-	msg['Subject']=u'Qunar Notice'
-	try:
-		smtp=smtplib.SMTP(MAIL_HOST) 
-		smtp.login(MAIL_USER,MAIL_PWD)
-		smtp.sendmail(FROM_ADDR,TO_ADDR,msg.as_string())
-		print 'sending mail success.'
-	except Exception as e:
-		print e
-	finally:
-		smtp.quit()
+from config import *
+# config_local.py will override the previous config.py
+try:
+    from config_local import *
+except:
+    pass
 
+def get_message(text):
+    msg = MIMEText(text, _charset='utf-8') 
+    msg['From'] = FROM_ADDR
+    msg['To'] = TO_ADDR
+    msg['Subject'] = u'Qunar Notice'
+    return msg
+
+def send(message):
+    try:
+        smtp = smtplib.SMTP(MAIL_HOST) 
+        smtp.login(MAIL_USER, MAIL_PWD)
+        smtp.sendmail(FROM_ADDR, TO_ADDR, msg.as_string())
+        print 'sending mail success.'
+    except Exception as e:
+        print e
+    finally:
+        smtp.quit()
+
+def send_email(text):
+    message = get_message(text)
+    send(message)
